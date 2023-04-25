c1c0b325be1ce108a48e7556d7c0ca40af3d5543
Kianayana <kianafillius@gmail.com>
2023-04-24 22:34:54 -0700

    Creating a basic Javascript file

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,23 @@
+//Variable Legend Below
+var RB="Today is my birthday! Let's celebrate.";
+var B=" How does the \"Happy Birthday\" song go?"
+ + " I don't know it since I'm only one day old. ";
+var D=" What else can I do with my life? ";
+var onef="I can watch a funny cat video", twof="I can play a video game.", threef="I can write a story";
+var Sent1="This is the beginning of the string";
+var Sent2=" and this is the end of the string."
+
+//Applying the Variables
+window.alert(RB);
+
+document.write(B + "<br>" + D);
+
+document.write("<br>");
+
+document.write(twof);
+
+document.write(10*10); //This number dones't mean anything. Just testing an expression.
+
+document.write("<br><br><br>");
+
+document.write(Sent1 + Sent2);
