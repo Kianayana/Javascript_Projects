13f134655212d4d9dda648269af8795392d10191
Kianayana <kianafillius@gmail.com>
2023-04-24 22:37:30 -0700

    Creating a basic Javascript file.

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main_2.js
@@ -0,0 +1,13 @@
+alert("I'm Alive!");
+
+window.alert(“Hello, world!”);
+
+document.write(‘Hello, world’);
+
+var A = “This is a string”;
+
+document.write(A);
+
+var RB = "Today is my birthday! Let's celebrate.";
+
+window.alert("RB");
