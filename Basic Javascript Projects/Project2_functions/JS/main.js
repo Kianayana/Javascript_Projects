9925292e47b2bab0ca1235fb172aa4dc0509f130
Kianayana <kianafillius@gmail.com>
2023-04-25 21:00:58 -0700

    Testing the use of  functions

---
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,11 @@
+function Fish1() {
+    var Type="Goldfish"; 
+    var Size="Small";
+    document.getElementById("GF").innerHTML = Type;
+}
+
+function Reason1() {
+    var sentence="They got along with the other fish"; 
+    sentence+=" and they looked extra cute" 
+    document.getElementById("Why").innerHTML = sentence;
+}
